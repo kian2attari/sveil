38bf99b043a5d95ddcc697a7444886de4fff84d2
Kian Attari <kianattari@college.harvard.edu>
2023-03-03 17:13:49 +0000

    typeform version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Typeform from "svelte-typeform-embed";
+  let typeformPopup;
+
+  function openPopup() {
+    return typeformPopup.open();
+  }
+
+  let value = 100;
+</script>
+
+
+<main>
+  <h1>sveil</h1>
+  <p>
+    The world's finest availability input platform 
+  </p>
+  
+
+  <div class="wrapper">
+    <Typeform url="https://u4sz0aw1y45.typeform.com/to/Gjun4Xv1" />
+  </div>
+
+  <div class="hidden">
+    <Typeform
+      url="https://u4sz0aw1y45.typeform.com/to/Gjun4Xv1"
+      bind:typeformPopup
+      popup
+      autoOpen={false} />
+  </div>
+
+  <button on:click={openPopup}>Click to open in popup mode</button>
+</main>
