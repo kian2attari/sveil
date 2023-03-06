8ab8cf6d80e2b760c6dc6e017c14ec81c1c9f327
Kian Attari <kianattari@college.harvard.edu>
2023-03-06 09:14:05 -0500

    quick css fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	}
+	// This breaks the email form, we can put it back if needed and I'll figure out how to fix it
+	// plugins: [require('@tailwindcss/forms')]
+};
